c94318c8297b5d5bc0be5d0c8c28ea5ba7f5feac
Stuart Rimel <srimel@pdx.edu>
2023-05-30 01:14:45 -0700

    Create item views from category button clicks

--- src/ui/category_grid.rs
+++ src/ui/category_grid.rs
@@ -12,7 +12,6 @@ pub fn build_category_grid(data_map: HashMap<String, Vec<String>>) -> ScrolledWi
     flowbox.set_selection_mode(gtk::SelectionMode::None);
     flowbox.set_max_children_per_line(3);
 
-    // Create category buttons from unique values in the "Category" column
     let categories = data_map.get("Category").unwrap();
     let unique_categories: HashSet<_> = categories.iter().cloned().collect();
 
@@ -40,10 +39,75 @@ pub fn build_category_grid(data_map: HashMap<String, Vec<String>>) -> ScrolledWi
         button.add_css_class(css_class);
         button.add_css_class("btn");
 
+        let cloned_data_map = data_map.clone();
+        let cloned_category = category.clone();
+        let scroll_window_clone = scroll_window.clone();
+
+        let flowbox_clone_for_back_button = flowbox.clone();
+
+
         // Connect a callback function to handle button click event
         button.connect_clicked(move |_| {
-            // Handle category button click event
-            println!("Category button clicked: {}", category);
+            // Here we will create a new FlowBox for the items
+            let item_flowbox = FlowBox::new();
+            item_flowbox.set_homogeneous(true);
+            item_flowbox.set_valign(gtk::Align::Start);
+            item_flowbox.set_selection_mode(gtk::SelectionMode::None);
+            item_flowbox.set_max_children_per_line(3);
+
+            println!("Cloned Category: {}", cloned_category);
+
+            if let (Some(categories), Some(items)) =
+                (cloned_data_map.get("Category"), cloned_data_map.get("Item"))
+            {
+                let mut item_css_class_iter = css_classes.iter().cycle();
+
+                // Filter items that belong to the cloned_category
+                let category_items: Vec<_> = items
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(index, item)| {
+                        if categories
+                            .get(index)
+                            .map_or(false, |c| c == &cloned_category)
+                        {
+                            Some(item)
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
+
+                println!("Items: {:?}", category_items);
+
+                for item in category_items {
+                    let item_button = Button::with_label(item);
+
+                    // Assign CSS class to item button
+                    let item_css_class = item_css_class_iter.next().unwrap();
+                    item_button.add_css_class(item_css_class);
+                    item_button.add_css_class("btn");
+
+                    item_flowbox.append(&item_button);
+                }
+
+                // Add a back button
+                let back_button = Button::with_label("Back");
+
+                // Assign CSS class to back button
+                back_button.add_css_class("btn");
+                item_flowbox.append(&back_button);
+
+                // Handle back button click
+                let flowbox_clone = flowbox_clone_for_back_button.clone();
+                let scroll_window_clone_back = scroll_window_clone.clone();
+                back_button.connect_clicked(move |_| {
+                    scroll_window_clone_back.set_child(Some(&flowbox_clone));
+                });
+            }
+
+            // Now replace the category flowbox with the item flowbox in the scroll window
+            scroll_window_clone.set_child(Some(&item_flowbox));
         });
 
         flowbox.append(&button);
